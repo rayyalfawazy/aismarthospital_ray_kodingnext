fb5c08a531672e5b8992a3660544f355168b3cc8
Rayyan Alfawazy Adheswarna <71803222+rayyalfawazy@users.noreply.github.com>
2026-06-25 11:04:36 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -134,6 +134,6 @@ with st.form("triage_form"):
   with col_age:
     age = st.number_input("Age", min_value=1, max_value=120, value=25)
   with col_gen:
-    gender = st.selectbox("Gender, options=['Female', 'Male'])
+    gender = st.selectbox("Gender", options=['Female', 'Male'])
   
   submitted = st.form_submit_button("Get AI Recomendation")
